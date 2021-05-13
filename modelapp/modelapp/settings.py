8c3042746032ffb19070560a95b2ed049478a34d
Amilya <amilya4u304@gmail.com>
2021-05-13 19:08:17 -0400

    third commit

--- modelapp/modelapp/settings.py
+++ modelapp/modelapp/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modelapp'
-]
+    'schoolchart',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
