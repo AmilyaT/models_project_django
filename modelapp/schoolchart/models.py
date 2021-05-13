90e73beb4c20ad9f8d8b9d879fa16e9a3a454c7a
Amilya <amilya4u304@gmail.com>
2021-05-13 17:43:28 -0400

    second commit

---
+++ modelapp/schoolchart/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from datetime import datetime
+
+# I'm creating my first models here!.
+class Student(models.Model):
+    full_name = models.CharField(max_length = 50)
+    year_of_graduation = models.DateField(default =datetime.today)
+    department = models.CharField(max_length= 50)
+    grade = models.IntegerField()
+    school = models.CharField(max_length = 50)
+    certifcate_type = models.CharField(max_length = 50)
+    
+
+class School(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Faculty(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Department(models.Model):
+    name = models.CharField(max_length = 50)
+    Faculty
+
+class Certificate_Type(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Grade(models.Model):
+    type = models.IntegerField()
