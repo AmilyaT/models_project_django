5f8b24caedc0f6a0fcb44616c0c5ed14aa3a6a7c
Amilya <amilya4u304@gmail.com>
2021-05-14 13:24:37 -0400

    updated

--- modelapp/schoolchart/models.py
+++ modelapp/schoolchart/models.py
@@ -1,28 +1,32 @@
 from django.db import models
-from datetime import datetime
+
+
+from django.db.models.deletion import CASCADE
 
 # I'm creating my first models here!.
-class Student(models.Model):
-    full_name = models.CharField(max_length = 50)
-    year_of_graduation = models.IntegerField()
-    department = models.ForeignKey(max_length= 50)
-    grade = models.ForeignKey()
-    school = models.ForeignKey(max_length = 50)
-    certifcate = models.ForeignKey(max_length = 50)
-    
-
 class School(models.Model):
     name = models.CharField(max_length = 50)
+    address = models.CharField(max_length= 50)
+    postal_code = models.IntegerField()
+
 
 class Faculty(models.Model):
     name = models.CharField(max_length = 50)
 
 class Department(models.Model):
     name = models.CharField(max_length = 50)
-    faculty = models.ForeignKey(max_length = 50)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
 class Certificate_Type(models.Model):
     name = models.CharField(max_length = 50)
 
 class Grade(models.Model):
     type = models.IntegerField()
+
+class Student(models.Model):
+    full_name = models.CharField(max_length = 50)
+    year_of_graduation = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    certifcate_type = models.ForeignKey(Certificate_Type, on_delete=models.CASCADE)
